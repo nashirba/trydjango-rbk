1fc93c4fb96d14b8568947f3df0e558c20ed63b6
nashirba <ashernur@gmail.com>
2020-09-11 14:26:03 +0600

    :sparkles: changed index to show images

--- app/unsplash/views.py
+++ app/unsplash/views.py
@@ -6,7 +6,7 @@ def get_request():
     base_url = 'https://api.unsplash.com/'
     UNSPLASH_ACCESS_KEY = '-RbPPqlmBdwSR5Clv8dMs8oCLfrK7KvB3um-dp7-swY'
     request_content = requests.get(
-        base_url + '/photos/random?count=10' + '&' + 'client_id=' + UNSPLASH_ACCESS_KEY
+        base_url + '/photos/random?count=2' + '&' + 'client_id=' + UNSPLASH_ACCESS_KEY
     )
     request_content = request_content.json()
 
@@ -15,4 +15,3 @@ def get_request():
 def index_view(request):
     request_json = get_request()
     return render(request, 'index.html', {'request_json':request_json})
-    
