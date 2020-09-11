24af627f69b314144daf9a1bac0335873804f467
nashirba <ashernur@gmail.com>
2020-09-11 14:02:29 +0600

    :hammer: created base.html changed index.html

--- app/unsplash/views.py
+++ app/unsplash/views.py
@@ -1,6 +1,18 @@
 import requests
 from django.shortcuts import render
 
-# Create your views here.
+
+def get_request():
+    base_url = 'https://api.unsplash.com/'
+    UNSPLASH_ACCESS_KEY = '-RbPPqlmBdwSR5Clv8dMs8oCLfrK7KvB3um-dp7-swY'
+    request_content = requests.get(
+        base_url + '/photos/random?count=10' + '&' + 'client_id=' + UNSPLASH_ACCESS_KEY
+    )
+    request_content = request_content.json()
+
+    return request_content
+
 def index_view(request):
-    return render(request, 'index.html')
+    request_json = get_request()
+    return render(request, 'index.html', {'request_json':request_json})
+    
