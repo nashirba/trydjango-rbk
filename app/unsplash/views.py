ddbaa6e5be17941ee424786a2339af42aa5b9477
nashirba <ashernur@gmail.com>
2020-09-11 14:36:11 +0600

    :lipstick: added bootstrap

--- app/unsplash/views.py
+++ app/unsplash/views.py
@@ -6,7 +6,7 @@ def get_request():
     base_url = 'https://api.unsplash.com/'
     UNSPLASH_ACCESS_KEY = '-RbPPqlmBdwSR5Clv8dMs8oCLfrK7KvB3um-dp7-swY'
     request_content = requests.get(
-        base_url + '/photos/random?count=2' + '&' + 'client_id=' + UNSPLASH_ACCESS_KEY
+        base_url + '/photos/random?count=10' + '&' + 'client_id=' + UNSPLASH_ACCESS_KEY
     )
     request_content = request_content.json()
 
