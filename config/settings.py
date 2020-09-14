5cef661c57bce0965a3deb79929b391d8c7f03a5
nashirba <ashernur@gmail.com>
2020-09-14 22:01:26 +0600

    :bento: created user app

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app.unsplash'
+    'app.unsplash',
+    'app.users'
 ]
 
 MIDDLEWARE = [
