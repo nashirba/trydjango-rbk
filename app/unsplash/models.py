07788b21cc6c20a96187e9082840c0df9a4fbd2b
nashirba <ashernur@gmail.com>
2020-09-17 13:02:58 +0600

    :hammer: changed photo model manytomany to user and removed user_date field

--- app/unsplash/models.py
+++ app/unsplash/models.py
@@ -3,11 +3,10 @@ from django.db import models
 
 
 class Photo(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    users = models.ManyToManyField(User)
     author = models.CharField(max_length=150)
     image = models.URLField(max_length=250)
     description = models.TextField()
-    author_created_date = models.DateTimeField()
     user_saved_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
